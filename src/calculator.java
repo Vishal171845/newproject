d94c42df141517464e74848dfecdc7b8c2860f14
Vishal171845 <vj2909045@gmail.com>
2022-03-15 16:39:46 +0530

    addition with function

---
+++ src/calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public int AdditionOperation(int x, int y)
+    {
+        int z = x + y;
+        return z;
+    }
+
+    public static void main(String[] args) {
+        calculator calc=new calculator();
+        Scanner input=new Scanner(System.in);
+        int n1,n2,result;
+        System.out.println("Enter num1");
+
+    }
+}
