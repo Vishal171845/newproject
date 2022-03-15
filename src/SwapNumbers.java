af3275721f57010e8035e592a2743851c7ece678
Vishal171845 <vj2909045@gmail.com>
2022-03-15 15:38:53 +0530

    calculator

---
+++ src/SwapNumbers.java
@@ -0,0 +1,11 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+        int  a = 20, b =50;
+
+        System.out.println("Before Swap");
+        System.out.println("a = " + a);
+        System.out.println("b = " +b);
+
+    }
+}
