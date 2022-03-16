e2333c0d0a1025160e9f3f6119f3da031ab373ad
Vishal171845 <vj2909045@gmail.com>
2022-03-16 14:34:52 +0530

    abstraction

--- src/calculator.java
+++ src/calculator.java
@@ -1,12 +1,7 @@
 import java.util.Scanner;
 
 public class calculator {
-
-    public int AdditionOperation(int x, int y)
-    {
-        int z = x + y;
-        return z;
-    }
+    
 
     public static void main(String[] args) {
         calculator calc=new calculator();
